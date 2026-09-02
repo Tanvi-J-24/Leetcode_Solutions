f8bda48e501e2156fa60215ac1a291348c9ca64b
Tanvi Dattatray Jadhav <tanvi.jadhav24@pccoepune.org>
2026-09-02 22:32:20 +0530

    Time: 71 ms (60.89%) | Memory: 111.2 MB (62.93%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mp;
+        unordered_set<int> st;
         for(int i=0; i<nums.size(); i++){
-            if(mp.find(nums[i]) != mp.end()){
+            if(st.find(nums[i]) != st.end()){
                 return true;
             }
-            else{
-                mp[nums[i]] = 1;
-            }
+            st.insert(nums[i]);
         }
 
         return false;
