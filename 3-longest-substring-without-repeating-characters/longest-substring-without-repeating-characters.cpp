0941ae54762c62e380d55b3edd970390512794f9
Tanvi Dattatray Jadhav <tanvi.jadhav24@pccoepune.org>
2026-09-05 23:52:28 +0530

    Time: 12 ms (64.12%) | Memory: 18.3 MB (60.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> st;
+        vector<int> last(128, -1);
         int left = 0;
         int ans = 0;
 
         for (int right = 0; right < s.size(); right++) {
-            while (st.count(s[right])) {
-                st.erase(s[left]);
-                left++;
+            if (last[s[right]] >= left) {
+                left = last[s[right]] + 1;
             }
 
-            st.insert(s[right]);
+            last[s[right]] = right;
             ans = max(ans, right - left + 1);
         }
 
